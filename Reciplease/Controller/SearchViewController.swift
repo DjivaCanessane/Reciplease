42f14e4ac57224b53c216a70776ed6ecc98ada9b
Canessane Djiveradjane <djiva.canessane@icloud.com>
2020-11-17 14:47:25 +0100

    add pods for alamoFire and gitignore

--- Reciplease/Controller/SearchViewController.swift
+++ Reciplease/Controller/SearchViewController.swift
@@ -8,5 +8,13 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    @IBOutlet weak var ingredientTextField: UITextField!
+    @IBOutlet weak var ingredientsTextView: UITextView!
+    
+    @IBAction func addIngredient(_ sender: UIButton) {
+    }
+    @IBAction func clearIngredients(_ sender: UIButton) {
+    }
+    @IBAction func searchRecipes(_ sender: UIButton) {
+    }
 }
