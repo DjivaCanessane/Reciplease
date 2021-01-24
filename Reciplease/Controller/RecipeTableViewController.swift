25fe7f3f0d34cda16866a416fcfa751739e0c0bb
Canessane Djiveradjane <djiva.canessane@icloud.com>
2021-01-24 18:54:53 +0100

    Empty message are now displayed when there is no data in RecipeTableViewController.

--- Reciplease/Controller/RecipeTableViewController.swift
+++ Reciplease/Controller/RecipeTableViewController.swift
@@ -12,19 +12,32 @@ class RecipeTableViewController: UITableViewController {
     var isComingFromSeachVC: Bool = false
     let recipeDataManager = ServiceContainer.recipeDataManager
     
-    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        loadData()
-//    }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         loadData()
     }
     
+    private func showEmptyMessage() {
+        //Empty messages
+        let networkErrorMessage = "No recipes found for those ingredients.\nPlease edit your ingredient and try again."
+        let coreDataEmptyMessage = "You have not any favorite recipes yet.\nTo add an favorite, pleace tap on the star button in the detailled recipe."
+        //Creation of empty message label
+        let label: UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: tableView.bounds.size.height))
+        label.numberOfLines = 0
+        label.text = self.isComingFromSeachVC ? networkErrorMessage : coreDataEmptyMessage
+        label.textColor = .black
+        label.textAlignment = .center
+        //Show refreshed table view with empty message
+        self.tableView.backgroundView = label
+        self.tableView.separatorStyle = .none
+        self.tableView.reloadData()
+    }
+    
     private func loadData() {
-        if  !isComingFromSeachVC {
+        //if isComingFromSeachVC = false, then fetch favorites recipesdata from CoreData
+        if !isComingFromSeachVC {
+            self.tableView.backgroundView = nil
+            self.tableView.separatorStyle = .singleLine
             let recipesData: [RecipeData] = recipeDataManager.all
             displayableRecipes = []
             for recipeData in recipesData {
@@ -34,13 +47,20 @@ class RecipeTableViewController: UITableViewController {
                     recipeURL: recipeData.recipeURL ?? "",
                     dishName: recipeData.name ?? "",
                     yield: Double(recipeData.yield),
-                    duration: Int(recipeData.duration),
+                    duration: recipeData.duration,
                     ingredients: recipeData.ingredients ?? []
                 )
                 displayableRecipes.append(displayableRecipe)
             }
+            
+        }
+        //show error when diplayableRecipes is empty
+        guard !displayableRecipes.isEmpty else {
+            return showEmptyMessage()
         }
         self.tableView.reloadData()
+        
+        
     }
     // MARK: - Table view data source
     
@@ -57,6 +77,7 @@ class RecipeTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         guard let recipeCell = tableView.dequeueReusableCell(withIdentifier: "recipeCell", for: indexPath) as? RecipeTableViewCell else {
             return UITableViewCell()
         }
